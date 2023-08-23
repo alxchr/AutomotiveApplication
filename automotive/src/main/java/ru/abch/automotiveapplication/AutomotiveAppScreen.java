1e7606b5bd3376f85148db54fa3aa527c07d1e36
alxchr <alxchr@gmail.com>
2023-08-23 12:21:49 +0500

    hello world

--- automotive/src/main/java/ru/abch/automotiveapplication/AutomotiveAppScreen.java
+++ automotive/src/main/java/ru/abch/automotiveapplication/AutomotiveAppScreen.java
@@ -2,8 +2,10 @@ package ru.abch.automotiveapplication;
 
 import androidx.annotation.NonNull;
 import androidx.car.app.CarContext;
+import androidx.car.app.CarToast;
 import androidx.car.app.Screen;
 import androidx.car.app.model.Action;
+import androidx.car.app.model.ActionStrip;
 import androidx.car.app.model.Pane;
 import androidx.car.app.model.PaneTemplate;
 import androidx.car.app.model.Row;
@@ -17,12 +19,18 @@ public class AutomotiveAppScreen  extends Screen {
     @Override
     public Template onGetTemplate() {
         Row row = new Row.Builder()
-                .setTitle("Thats our Grape App!").build();
+                .setTitle("Thats our Automotive App!").build();
+        Action action = new Action.Builder()
+                .setOnClickListener(
+                        () -> CarToast.makeText(getCarContext(), "Hello!", CarToast.LENGTH_SHORT).show())
+                .setTitle("Say hi!")
+                .build();
 
+        ActionStrip actionStrip = new ActionStrip.Builder().addAction(action).build();
         return new PaneTemplate.Builder(
                 new Pane.Builder()
                         .addRow(row)
                         .build()
-        ).setHeaderAction(Action.APP_ICON).build();
+        ).setActionStrip(actionStrip).setHeaderAction(Action.APP_ICON).build();
     }
 }
